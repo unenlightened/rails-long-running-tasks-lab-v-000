0ce27c31a31a6db093be00ee2073d8a6e163047b
Chely Ho <adept_inept@yahoo.com>
2018-07-14 22:28:43 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
 class SongsController < ApplicationController
-
+  require 'csv'
+  
   def index
     @songs = Song.all
   end
@@ -43,6 +44,10 @@ class SongsController < ApplicationController
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
+  end
+
+  def upload
+
   end
 
   private
